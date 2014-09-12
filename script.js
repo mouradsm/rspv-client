ac9e9aaa0a2e6eb22f5dc1dbb24d59ba87267207
Diego Moura <diegosouza20049@hotmail.com>
2014-09-11 22:06:52 -0300

    Refatoração para atender a todos os tipos de tags

--- script.js
+++ script.js
@@ -1,11 +1,97 @@
+'use strict';
 (function () {
     var app = angular.module('rspv', []);
 
 
-    app.controller('OrderFormController', function ($scope, $http) {
+
+    app.controller('ConvidadoController', function ($scope, $http) {
+
+        $scope.convidados = [];
+        $scope.confirmados = [];
+
+        function processaConvidados (convidados){
+            var objConvidado = convidados[0];
+
+            var tag = processaTag(objConvidado.Tag)
+
+            var lastItem = tag.slice(-1);
+
+            if((lastItem == 'Família') || (lastItem == 'Esposo')  ){
+                tag.pop();
+            }
+
+            var auxAcompanhantes = objConvidado.Acompanhantes;
+
+            objConvidado.Acompanhantes = [];
+
+            angular.forEach(auxAcompanhantes, function (o) {
+                objConvidado.Acompanhantes.unshift({ nome: o});
+            })
+
+            angular.forEach(tag, function (o) {
+                objConvidado.Acompanhantes.unshift({ nome: o});
+            });
+
+            return objConvidado.Acompanhantes;
+        };
+
+        function processaTag(tag){
+            var arrTag = tag.split(' e ');
+            return arrTag;
+        }
+
+        $scope.total = function(){
+            var total = 0;
+
+            // Uso o método auxiliar do Angular 'forEach'
+            // para iterar o array services
+
+            angular.forEach($scope.convidados, function (s) {
+
+                if (s.active) {
+                    if ($.inArray(s.nome, $scope.confirmados) == -1) {
+                        $scope.confirmados.unshift(s.nome);
+                    }
+                    total += 1;
+                } else {
+                    if ($.inArray(s.nome, $scope.confirmados) != -1) {
+                        $scope.confirmados.pop(s.nome);
+                    }
+                }
+            });
+
+            return total;
+        }
+        $scope.toggleActive = function (s) {
+            s.active = !s.active;
+        };
+
+        $scope.getConvidado = function(tag){
+            if(typeof tag == 'string'){
+                
+                var url     = 'http://hidden-refuge-3353.herokuapp.com/api//convidado/' + tag;
+                var method  = 'GET';
+
+                $http({
+                    url     : url,
+                    method  : method
+                }).
+                success(function(data){
+                    //console.log('Data: ',data);
+                    $scope.convidados = processaConvidados(data);
+                }).
+                error(function(err){
+                    console.log('Erro: ',err);
+                })
+            }
+        }
+    });
 
 
-        $scope.$on('LOAD', function(){$scope.loading=true});
+
+
+
+/*        $scope.$on('LOAD', function(){$scope.loading=true});
         $scope.$on('UNLOAD', function(){$scope.loading=false});
 
         $scope.Convidado = [];
@@ -120,7 +206,7 @@
         };
 
 
-    });
+    });*/
 
 })();
 
